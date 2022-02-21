9cf3f961e2db38897fa7963ce62d0d4e7beaa695
orjanskotnes <oskotnes@gmail.com>
2022-02-21 10:48:43 +0100

    Server works, global script fixed. Tests next

--- server/server.js
+++ server/server.js
@@ -1,1 +1,27 @@
-console.log("Hello world!");
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use(express.static(path.resolve("../client/dist")));
+
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    res.sendStatus(404);
+    next();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
